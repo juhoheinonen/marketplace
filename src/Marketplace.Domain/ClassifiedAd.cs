a0dc7222d5fe38970cd4d9a258c6e4cd2864602f
Juho A. Heinonen <juho.heinonen@nortal.com>
2021-07-01 09:36:29 +0300

    Continue:

--- src/Marketplace.Domain/ClassifiedAd.cs
+++ src/Marketplace.Domain/ClassifiedAd.cs
@@ -4,21 +4,31 @@ namespace Marketplace.Domain
 {
     public class ClassifiedAd
     {
-        public Guid Id { get; private set; }
+        public Guid Id { get; }
 
-        public ClassifiedAd(Guid id)
+        public ClassifiedAd(Guid id, Guid ownerId)
         {
             if (id == default)
             {
                 throw new ArgumentException("Identity must be specified", nameof(id));
             }
 
+            if (ownerId == default) {
+                throw new ArgumentException("Owner id must be specified", nameof(ownerId));
+            }
+
             Id = id;
         }
+
+        public void SetTitle(string title) => _title = title;
+
+        public void UpdateText(string text) => _text = text;
+
+        public void UpdatePrice(decimal price) => _price = price;
 
         private Guid _ownerId;
         private string _title;
         private string _text;
-        private string _price;
+        private decimal _price;
     }
 }
