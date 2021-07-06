2709bab7f7fc21b306525af22db85bd72e90d6f5
Juho A. Heinonen <juho.heinonen@nortal.com>
2021-07-06 15:46:36 +0300

    Continue.

--- src/Marketplace/Startup.cs
+++ src/Marketplace/Startup.cs
@@ -11,6 +11,7 @@ namespace Marketplace
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
             services.AddSwaggerGen(c =>
                 c.SwaggerDoc("v1",
                     new Microsoft.OpenApi.Models.OpenApiInfo
