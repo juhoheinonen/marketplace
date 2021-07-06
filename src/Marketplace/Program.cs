874cc68db89d00e1e59af5f4317b3a61b61e35fb
Juho A. Heinonen <juho.heinonen@nortal.com>
2021-07-06 15:37:46 +0300

    Continue.

--- src/Marketplace/Program.cs
+++ src/Marketplace/Program.cs
@@ -1,48 +1,7 @@
-// using System.IO;
-// using Microsoft.AspNetCore.Hosting;
-// using Microsoft.Extensions.Configuration;
-// using Microsoft.Extensions.Hosting;
-// using static System.Environment;
-// using static System.Reflection.Assembly;
-
-// namespace Marketplace
-// {
-//     public static class Program
-//     {
-//         static Program() =>
-//             CurrentDirectory = Path.GetDirectoryName(GetEntryAssembly().Location);
-
-//         public static void Main(string[] args)
-//         {
-//             var configuration = BuildConfiguration(args);
-
-//             ConfigureWebHost(configuration).Build().Run();
-//         }
-
-//         private static IConfiguration BuildConfiguration(string[] args)
-//             => new ConfigurationBuilder()
-//                 .SetBasePath(CurrentDirectory)
-//                 .Build();
-
-//         private static IHostBuilder ConfigureWebHost(
-//             IConfiguration configuration)
-//             => Host.CreateDefaultBuilder()
-//                 .UseStartup<Startup>()
-//                 .UseConfiguration(configuration)
-//                 .UseContentRoot(CurrentDirectory)
-//                 .UseKestrel();
-//     }
-// }
-
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using static System.Reflection.Assembly;
 using static System.Environment;
 
