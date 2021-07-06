3acfde029f62945699d00584141cbfd9ade0a297
Juho A. Heinonen <juho.heinonen@nortal.com>
2021-07-06 13:32:28 +0300

    Modify Entity class.

--- src/Marketplace.Framework/Entity.cs
+++ src/Marketplace.Framework/Entity.cs
@@ -3,15 +3,25 @@ using System.Linq;
 
 namespace Marketplace.Framework
 {
-    public abstract class Entity {
+    public abstract class Entity
+    {
         private readonly List<object> _events;
 
         protected Entity() => _events = new List<object>();
 
-        protected void Raise(object @event) => _events.Add(@event);
-
+        protected void Apply(object @event)
+        {
+            When(@event);
+            EnsureValidState();
+            _events.Add(@event);            
+        }
+        
         public IEnumerable<object> GetChanges() => _events.AsEnumerable();
 
         public void ClearChanges() => _events.Clear();
+
+        protected abstract void When(object @event);
+
+        protected abstract void EnsureValidState();
     }
 }
