d204c91457e17a81b48381958225c48e5cd7e5cc
Juho A. Heinonen <juho.heinonen@nortal.com>
2021-07-06 15:14:45 +0300

    Continue.

--- src/Marketplace/Program.cs
+++ src/Marketplace/Program.cs
@@ -1,26 +1,75 @@
+// using System.IO;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.Hosting;
+// using static System.Environment;
+// using static System.Reflection.Assembly;
+
+// namespace Marketplace
+// {
+//     public static class Program
+//     {
+//         static Program() =>
+//             CurrentDirectory = Path.GetDirectoryName(GetEntryAssembly().Location);
+
+//         public static void Main(string[] args)
+//         {
+//             var configuration = BuildConfiguration(args);
+
+//             ConfigureWebHost(configuration).Build().Run();
+//         }
+
+//         private static IConfiguration BuildConfiguration(string[] args)
+//             => new ConfigurationBuilder()
+//                 .SetBasePath(CurrentDirectory)
+//                 .Build();
+
+//         private static IHostBuilder ConfigureWebHost(
+//             IConfiguration configuration)
+//             => Host.CreateDefaultBuilder()
+//                 .UseStartup<Startup>()
+//                 .UseConfiguration(configuration)
+//                 .UseContentRoot(CurrentDirectory)
+//                 .UseKestrel();
+//     }
+// }
+
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using static System.Reflection.Assembly;
+using static System.Environment;
 
 namespace Marketplace
 {
     public class Program
     {
+
+        static Program() => CurrentDirectory = Path.GetDirectoryName(GetEntryAssembly().Location);
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = BuildConfiguration(args);
+            CreateHostBuilder(configuration).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        private static IConfiguration BuildConfiguration(string[] args) =>
+            new ConfigurationBuilder().SetBasePath(CurrentDirectory).Build();
+
+        public static IHostBuilder CreateHostBuilder(IConfiguration configuration) =>
+            Host.CreateDefaultBuilder()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseContentRoot(CurrentDirectory);
+                    webBuilder.UseConfiguration(configuration);
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseKestrel();
                 });
     }
 }
